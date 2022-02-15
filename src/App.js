2e92d164340bf35fe1b78cc82ef643d04dbaf588
Joshua Smith <joshua.smith@generalassemb.ly>
2022-02-15 10:05:47 -0600

    updated jsx and added gif images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+    const [gifs, setGifs] = useState([]);
+	
+    useEffect(() => {
+		function getGifData() {
+			const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_KEY}&q=cats&limit=25&rating=G&lang=en`;
+
+			fetch(url)
+				.then((res) => res.json())
+				.then((res) => {
+					console.log('We have data!', res.data);
+                    // we are not worrying about rendering - we just want to test that the api key is operational
+					setGifs(res.data);
+				})
+				.catch(console.error);
+		}
+		getGifData();
+	}, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Testing Netlify Deployment</h1>
+        {
+            gifs.map(gif=><img key={gif.id} src={gif.images.downsized_medium.url} />)
+        }
       </header>
     </div>
   );
